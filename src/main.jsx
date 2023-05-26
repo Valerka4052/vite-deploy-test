f70f8fca362866cac68806b8b3fc39e3bd5f67f4
Valerka4052 <114054150+Valerka4052@users.noreply.github.com>
2023-05-26 19:55:17 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import store from './redux/store.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <BrowserRouter basename='vite-deploy-test' >
+    <BrowserRouter >
       <Provider store={store}>
         <App />
         </Provider>
